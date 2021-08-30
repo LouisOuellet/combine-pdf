0e3898e4c3bbeac05939d2de965662e349c3c237
Louis Ouellet <louis@TheMacSwitch.local>
2021-08-30 11:43:55 -0400

    UPDATE

---
+++ compress.php
@@ -0,0 +1,12 @@
+<?php
+
+// Import Librairies
+require_once dirname(__FILE__) . '/src/lib/pdf.php';
+
+// Adding Librairies
+$PDF = new apiPDF();
+
+$file = dirname(__FILE__) . '/tmp/test.php';
+$size = 10000000; //10mb
+$compressed = $PDF->compress($file,$size);
+var_dump($compressed);
