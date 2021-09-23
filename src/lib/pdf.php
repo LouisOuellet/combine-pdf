8605790296b828b3e45417371b1b74925ad43781
Louis Ouellet <louis@TheMacSwitch.local>
2021-09-23 19:22:38 -0400

    UPDATE

--- src/lib/pdf.php
+++ src/lib/pdf.php
@@ -1,44 +1,68 @@
 <?php
 
 // Import Librairies
+require_once dirname(__FILE__,3) . '/vendor/fpdf183/fpdf.php';
+require_once dirname(__FILE__,3) . '/vendor/FPDI-2.3.6/src/FpdiException.php';
+require_once dirname(__FILE__,3) . '/vendor/FPDI-2.3.6/src/PdfParser/PdfParserException.php';
+require_once dirname(__FILE__,3) . '/vendor/FPDI-2.3.6/src/PdfParser/CrossReference/CrossReferenceException.php';
+require_once dirname(__FILE__,3) . '/vendor/FPDI-2.3.6/src/autoload.php';
+require_once dirname(__FILE__,3) . '/vendor/php-pdf-merge/src/Jurosh/PDFMerge/PDFObject.php';
+require_once dirname(__FILE__,3) . '/vendor/php-pdf-merge/src/Jurosh/PDFMerge/PDFMerger.php';
+require_once dirname(__FILE__,3) . '/vendor/pdf-version-converter-1.0.5/src/Converter/ConverterInterface.php';
+require_once dirname(__FILE__,3) . '/vendor/pdf-version-converter-1.0.5/src/Converter/GhostscriptConverter.php';
+require_once dirname(__FILE__,3) . '/vendor/pdf-version-converter-1.0.5/src/Converter/GhostscriptConverterCommand.php';
+require_once dirname(__FILE__,3) . '/vendor/pdf-version-converter-1.0.5/src/Guesser/GuesserInterface.php';
+require_once dirname(__FILE__,3) . '/vendor/pdf-version-converter-1.0.5/src/Guesser/RegexGuesser.php';
+require_once dirname(__FILE__,3) . '/vendor/symfony/process/Process.php';
+require_once dirname(__FILE__,3) . '/vendor/symfony/process/ProcessUtils.php';
+require_once dirname(__FILE__,3) . '/vendor/symfony/process/Pipes/PipesInterface.php';
+require_once dirname(__FILE__,3) . '/vendor/symfony/process/Pipes/AbstractPipes.php';
+require_once dirname(__FILE__,3) . '/vendor/symfony/process/Pipes/UnixPipes.php';
+require_once dirname(__FILE__,3) . '/vendor/symfony/filesystem/Filesystem.php';
 
 // import the namespaces
+use Symfony\Component\Filesystem\Filesystem;
+use Xthiago\PDFVersionConverter\Guesser\RegexGuesser;
+use Xthiago\PDFVersionConverter\Converter\GhostscriptConverterCommand;
+use Xthiago\PDFVersionConverter\Converter\GhostscriptConverter;
 
-class apiPDF {
+class apiPDF{
 
-	protected $DPI = 300;
-	protected $scale = 80;
-	protected $maxFileSize = 10000000;
+	public function __construct(){}
 
-	private $compression = false;
-
-	public $errors = [];
-
-	public function __construct($settings = null){
-		if($settings != null && is_array($settings)){
-			if(isset($settings['scale'])){ $this->scale = $settings['scale']; }
-			if(isset($settings['maxFileSize'])){ $this->maxFileSize = $settings['maxFileSize']; }
-			if(isset($settings['compression'])){ $this->compression = $settings['compression']; }
-		}
+	public function version($file){
+		$guesser = new RegexGuesser();
+		return $guesser->guess($file);
 	}
 
-	public function merge($files, $size = null){
-		if($size == null || !is_numeric($size)){ $size = $this->maxFileSize; }
-		// Verifications
-		if(is_array($files) && count($files) > 0){
-			// Initialize PDF
-			$dir = pathinfo($files[0])['dirname'];
-			$filename = $dir.'/'.time().'.pdf';
-			$cmd = "convert ";
-			foreach($files as $file){
-				if(strpos(strtolower($file), '.pdf') !== false){
-					$decrypted = str_replace('.pdf','-decrypted.pdf',$file);
-					shell_exec("qpdf --decrypt $file $decrypted");
-					$cmd .= $decrypted." ";
-				}
+	public function convert($file){
+		$command = new GhostscriptConverterCommand();
+		$filesystem = new Filesystem();
+		$converter = new GhostscriptConverter($command, $filesystem);
+		$converter->convert($file, '1.4');
+	}
+
+	public function combine($files,$destDIR, $filename = null){
+
+		// Create Merger Instance
+		$pdf = new \Jurosh\PDFMerge\PDFMerger;
+
+		// Start Merging
+		foreach($files as $file){
+			if(strpos(strtolower($file), '.pdf') !== false){
+				if($this->version($file) != '1.14'){ $this->convert($file); }
+				$pdf->addPDF($file, 'all');
 			}
-			shell_exec($cmd." $filename");
-			return $filename;
-		} else { $this->errors[] =  "No Files!"; }
+		}
+
+		// Generate Name
+		if($filename == null){ $file = trim($destDIR,'/').'/'.time().'.pdf'; }
+		else{ $file = '/'.trim($destDIR,'/').'/'.$filename.'.pdf'; }
+
+		// Save Locally
+		$pdf->merge('file', $file);
+
+		// Return
+		return $file;
 	}
 }
